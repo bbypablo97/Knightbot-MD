c832ec76d0fad7870a907e2bb418b2abfd909910
bbypablo97 <theophilusanabila@gmail.com>
2025-06-01 14:48:31 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,10 +2,10 @@ const settings = {
   packname: 'Knight Bot',
   author: '‎Bot',
   botName: "Knight Bot",
-  botOwner: 'Knight Bot', // Your name
-  ownerNumber: '14703105589', //Set your number here without + symbol, just add country code & number without any space
+  botOwner: 'Bby Pablo', // Your name
+  ownerNumber: '233597524238', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
-  commandMode: "public",
+  commandMode: "pravite",
   description: "This is a bot for managing group commands and automating tasks.",
   version: "2.0.0",
 };
